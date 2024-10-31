523a0f336df2c8309cd7046eb1e5a3f97978217f
vishalBelwal <vishalbelwal52@gmail.com>
2024-10-31 12:52:50 +0530

    inits

---
+++ constants/index.ts
@@ -0,0 +1,28 @@
+const sideBarLinks = [
+    {
+        label: 'Home',
+        route: '/',
+        imgURL: '/icons/Home.svg'
+    },
+    {
+        label: 'Upcoming Meetings',
+        route: '/upcoming-meetings',
+        imgURL: '/icons/upcoming.svg'
+    },
+    {
+        label: 'Previous Meetings',
+        route: '/previous-meetings',
+        imgURL: '/icons/previous.svg'
+    },
+    {
+        label: 'Recordings',
+        route: '/recordings',
+        imgURL: '/icons/Video.svg'
+    },
+    {
+        label: 'Personal Room',
+        route: '/personal-room',
+        imgURL: '/icons/add-personal.svg'
+    }
+]
+export default sideBarLinks
