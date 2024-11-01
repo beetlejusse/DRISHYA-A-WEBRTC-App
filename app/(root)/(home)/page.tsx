2571a2d5dec3c8672ba719e7ef9631940108d91b
vishalBelwal <vishalbelwal52@gmail.com>
2024-11-01 20:51:41 +0530

    random change

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,12 +1,9 @@
-'use client'
 import React from 'react'
 
-const Home = () => {
+export default function Home () {
   return (
     <div className='flex size-fll flex-col gap-10 text-white'>
       <div className='h-[300px] w-full bg-hero bg-cover'></div>
     </div>
   )
 }
-
-export default Home
