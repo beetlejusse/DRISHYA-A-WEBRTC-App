8b33d1bc85e511f1ee1ffbb3c2a25c0ecd468758
vishalBelwal <vishalbelwal52@gmail.com>
2024-11-15 14:56:36 +0530

    ui components

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 const Loader = () => {
   return (
     <div className="flex-center h-screen w-full">
-        <Loader2 />
+        <span className="loading loading-spinner loading-lg"></span>
     </div>
   )
 }
