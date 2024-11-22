ffc43a382285d2f0317e2e1901914011837c2bee
vishalBelwal <vishalbelwal52@gmail.com>
2024-11-23 00:53:50 +0530

    recordings

--- app/(root)/(home)/recordings/page.tsx
+++ app/(root)/(home)/recordings/page.tsx
@@ -1,11 +1,14 @@
+import { CallList } from '@/components/CallList'
 import React from 'react'
 
 export default function Recordings  ()  {
   return (
     <div className='flex size-fll flex-col gap-10 text-white'>
       <h1 className='text-3xl font-bold'>
-        recordings
+        Recordings
       </h1>
+
+      <CallList type='recordings' />
     </div>
   )
 }
