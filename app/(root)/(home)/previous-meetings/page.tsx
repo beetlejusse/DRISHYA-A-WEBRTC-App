b9552818a6699d322fb1fe2d053fbcc84f2420c1
vishalBelwal <vishalbelwal52@gmail.com>
2024-11-23 00:53:41 +0530

    previous meeting

--- app/(root)/(home)/previous-meetings/page.tsx
+++ app/(root)/(home)/previous-meetings/page.tsx
@@ -1,11 +1,14 @@
+import { CallList } from '@/components/CallList'
 import React from 'react'
 
 export default function Previousmeetings () {
   return (
     <div className='flex size-fll flex-col gap-10 text-white'>
       <h1 className='text-3xl font-bold'>
-        Previous meetings
+        Previous Meetings
       </h1>
+
+      <CallList type='endedMeeting' />
     </div>
-  )
+)
 }
