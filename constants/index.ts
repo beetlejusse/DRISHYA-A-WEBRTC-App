eef261888d551f3c33485d158d3a9cb82227ac5d
beetlejusse <vishalbelwal52@gmail.com>
2024-12-27 20:01:58 +0530

    constants

--- constants/index.ts
+++ constants/index.ts
@@ -1,27 +1,25 @@
+const randomValue = Math.random().toString(36).substring(2, 8);
+const timestamp = new Date().getTime();
+export const randomLink = `?sessionId=${randomValue}&timestamp=${timestamp}`
 const sideBarLinks = [
     {
-        label: 'Home',
-        route: '/',
-        imgURL: '/icons/Home.svg'
-    },
-    {
         label: 'Upcoming Meetings',
-        route: '/upcoming-meetings',
+        route: `/upcoming-meetings${randomLink}`,
         imgURL: '/icons/upcoming.svg'
     },
     {
         label: 'Previous Meetings',
-        route: '/previous-meetings',
+        route: `/previous-meetings${randomLink}`,
         imgURL: '/icons/previous.svg'
     },
     {
         label: 'Recordings',
-        route: '/recordings',
+        route: `/recordings${randomLink}`,
         imgURL: '/icons/Video.svg'
     },
     {
         label: 'Personal Room',
-        route: '/personal-room',
+        route: `/personal-room${randomLink}`,
         imgURL: '/icons/add-personal.svg'
     }
 ]
