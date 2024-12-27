a07a9043bee2e993d0d803871000c7b0af04983f
beetlejusse <vishalbelwal52@gmail.com>
2024-12-27 19:58:45 +0530

    temporary navbar component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,7 +8,7 @@ export const Navbar = () => {
 
   return (
     <nav className="flex flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10">
-      <Link href="/" className="flex items-center gap-1">
+      <Link href="/home" className="flex items-center gap-1">
         <p className="text-[25px] font-extrabold text-white ">DRISHYA</p>
       </Link>
 
