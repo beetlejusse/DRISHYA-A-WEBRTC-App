bad31982deb5c3ca6b7b2b2c477f856803eb1a57
beetlejusse <vishalbelwal52@gmail.com>
2025-01-02 16:19:07 +0530

    auth page changes

---
+++ app/auth/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,90 @@
+import { SignUp } from '@clerk/nextjs'
+import React, { useCallback } from 'react'
+import Particles from 'react-tsparticles'
+import { Engine } from 'tsparticles-engine'
+import { loadSlim } from 'tsparticles-slim'
+
+export default function SignUpPage() {
+  const particlesInit = useCallback(async (engine: Engine) => {
+        await loadSlim(engine)
+      }, [])
+  return (
+    <main className='flex h-screen w-full flex-center'>
+      <Particles
+                    id="tsparticles"
+                    init={particlesInit}
+                    options={{
+                      background: {
+                        color: {
+                          value: "#000000",
+                        },
+                      },
+                      fpsLimit: 120,
+                      interactivity: {
+                        events: {
+                          onClick: {
+                            enable: true,
+                            mode: "push",
+                          },
+                          onHover: {
+                            enable: true,
+                            mode: "repulse",
+                          },
+                          resize: true,
+                        },
+                        modes: {
+                          push: {
+                            quantity: 4,
+                          },
+                          repulse: {
+                            distance: 200,
+                            duration: 0.4,
+                          },
+                        },
+                      },
+                      particles: {
+                        color: {
+                          value: "#ffffff",
+                        },
+                        links: {
+                          color: "#ffffff",
+                          distance: 150,
+                          enable: true,
+                          opacity: 0.5,
+                          width: 1,
+                        },
+                        move: {
+                          direction: "none",
+                          enable: true,
+                          outModes: {
+                            default: "bounce",
+                          },
+                          random: false,
+                          speed: 1,
+                          straight: false,
+                        },
+                        number: {
+                          density: {
+                            enable: true,
+                            area: 800,
+                          },
+                          value: 80,
+                        },
+                        opacity: {
+                          value: 0.5,
+                        },
+                        shape: {
+                          type: "circle",
+                        },
+                        size: {
+                          value: { min: 1, max: 5 },
+                        },
+                      },
+                      detectRetina: true,
+                    }}
+                    className="absolute inset-0"
+                  />
+        <SignUp/>
+    </main>
+  )
+}
