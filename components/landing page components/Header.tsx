f2945d2d5c5971f3d93b55b2c7a1ed1e326b80a1
beetlejusse <vishalbelwal52@gmail.com>
2025-01-02 16:33:26 +0530

    modiified navbar component on main page

--- components/landing page components/Header.tsx
+++ components/landing page components/Header.tsx
@@ -8,9 +8,9 @@ const Header = () => {
   const [isOpen, setIsOpen] = useState(false)
 
   return (
-    <header className="fixed w-full z-10 bg-black bg-opacity-50 backdrop-blur-sm">
+    <header className="fixed w-full z-50 bg-black bg-opacity-50  backdrop-blur-sm">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex items-center justify-between h-16">
+        <div className="flex items-center justify-between h-28 md:h-20">
           <div className="flex items-center">
             <Link href="#" className="text-2xl font-bold text-purple-500">DRISHYA</Link>
           </div>
