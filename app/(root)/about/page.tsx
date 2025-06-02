4aa66d6d2a0db1ddbdb2c7c35fddb084798023d6
vishalBelwal <vishalbelwal52@gmail.com>
2025-06-02 20:07:18 +0530

    random ui changes | changed color scheming of application

--- app/(root)/about/page.tsx
+++ app/(root)/about/page.tsx
@@ -3,9 +3,7 @@ import React from 'react';
 
 export default function AboutPage() {
   return (
-    <main className="relative flex h-screen w-full items-center justify-center">
-      <div className="absolute inset-0 animate-pulse bg-opacity-10"></div>
-
+    <main className="relative min-h-screen flex w-full items-center justify-center">
       <div className="z-10 flex flex-col items-center justify-center p-6 rounded-lg bg-black bg-opacity-40 backdrop-blur-lg shadow-lg max-w-3xl">
         <h1 className="text-white text-4xl font-bold mb-4">About DRISHYA</h1>
         <p className="text-gray-300 mb-6 text-center">
@@ -18,7 +16,7 @@ export default function AboutPage() {
           href="https://github.com/beetlejusse/WEBRTC"
           target="_blank"
           rel="noopener noreferrer"
-          className="mt-4 inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-purple-600 hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500"
+          className="mt-4 inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-lg text-white bg-inherit focus:outline-none focus:ring-2 focus:ring-offset-2 "
         >
           Star on GitHub
         </a>
--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -5,7 +5,7 @@ import React, { ReactNode } from "react";
 const HomeLayout = ({ children }: { children: ReactNode }) => {
   return (
     <StreamvideoProvider>
-      <main className="relative bg-background">
+      <main className="relative bg-[#0a1119] bg-[radial-gradient(circle_at_bottom_left,rgba(13,148,136,0.15),transparent_40%),radial-gradient(circle_at_top_right,rgba(45,212,191,0.1),transparent_30%)]">
         <Navbar />
         <div className="flex">
           <section className="min-h-screen flex-1 px-6 pb-6 pt-24 transition-all duration-300 ease-in-out max-md:pb-14 sm:px-8 lg:px-12">
